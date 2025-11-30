81fb61672db36914619ee8134cb4bb89399c03bb
Erik Olvera <erikolvera44@gmail.com>
2025-11-30 00:55:27 -0600

    used the completed functions to get the data from the cs4346-data folder

--- main.py
+++ main.py
@@ -3,13 +3,12 @@
 # digits
 #5000 lines of digits training labels
 #140000 lines of digits training images
-# total height = 14000/5000 = 28 lines per image
+# total height = 140000/5000 = 28 lines per image
 
 # faces
 #451 lines face data training labels
 # 31570 lines of face data train
-# so total height = 31580 / 451 = 70 lines per face
-
+# so total height = 31570 / 451 = 70 lines per face
 
 
 #wanna read the labels first. theyre basic integers
@@ -19,6 +18,7 @@ def read_labels(labels_file):
         labels_content = f.read().splitlines()
         for line in labels_content:
             labels.append(int(line))
+    return labels
 
 # want to read the images
 def read_images(images_files, total_images):
@@ -34,6 +34,31 @@ def read_images(images_files, total_images):
         one_image = images_content[i:i+height]
         if len(one_image) == height:
             images.append(one_image)
-    return images
+    return images, height
+
+# digits
+digit_train_labels =read_labels("cs4346-data/digitdata/traininglabels")
+digit_validate_labels = read_labels("cs4346-data/digitdata/validationlabels")
+digit_test_labels = read_labels("cs4346-data/digitdata/testlabels")
+
+digit_train_images, digit_height = read_images("cs4346-data/digitdata/trainingimages", total_images=len(digit_train_labels))
+digit_valid_images, _ = read_images("cs4346-data/digitdata/validationimages", total_images=len(digit_validate_labels))
+digit_test_images, _ = read_images("cs4346-data/digitdata/testimages",total_images=len(digit_test_labels))
+
+# faces
+
+face_train_labels = read_labels("cs4346-data/facedata/facedatatrainlabels")
+face_validate_labels = read_labels("cs4346-data/facedata/facedatavalidationlabels")
+face_test_labels  = read_labels("cs4346-data/facedata/facedatatestlabels")
+
+face_train_images, image_height = read_images("cs4346-data/facedata/facedatatrain", total_images=len(face_train_labels))
+face_valid_images, _ = read_images("cs4346-data/facedata/facedatavalidation", total_images=len(face_validate_labels))
+face_test_images, _ = read_images("cs4346-data/facedata/facedatatest", total_images=len(face_test_labels))
 
 
+# making sure code actually works
+# print(digit_height)
+# print(len(digit_train_images), len(digit_train_labels))
+#
+# print(image_height)
+# print(len(face_train_images),len(face_train_labels))
